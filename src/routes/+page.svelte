f73b5868328e8413f4891ad4f89c6ec47fb2a2f6
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 12:07:11 -0400

    heartrate thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import HeartRateInput from "./HeartRateInput.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<HeartRateInput />
