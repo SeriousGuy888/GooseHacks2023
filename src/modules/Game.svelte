e1d5b1832210478853ef1f9e787ba9fbe6d3f892
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 12:31:18 -0400

    time since start

--- src/modules/Game.svelte
+++ src/modules/Game.svelte
@@ -1,9 +1,40 @@
 <script>
   import { heartRate, points } from "$lib/store";
+
+  let gameStarted = false;
+  let timeStarted = 0;
+
+  function startGame() {
+    points.set(0);
+    timeStarted = Date.now();
+
+    gameStarted = true;
+  }
+
+  let now = Date.now();
+  setInterval(() => {
+    now = Date.now();
+  }, 1000);
+
+  $: secondsSinceStart = Math.floor((now - timeStarted) / 1000);
 </script>
 
 <section class="p-4 bg-slate-300 rounded-md">
   <h1>This is the game part</h1>
-  <p>reading the heartrate from a different component: {$heartRate}</p>
-  <p>Points: {$points}</p>
+
+  {#if gameStarted}
+    <div class="grid mt-4 gap-2">
+      <p>reading the heartrate from a different component: {$heartRate}</p>
+      <p>Points: {$points}</p>
+      <p>Seconds since start: {secondsSinceStart}</p>
+      <hr />
+    </div>
+  {:else}
+    <button
+      class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      on:click={startGame}
+    >
+      Start Game
+    </button>
+  {/if}
 </section>
