f1eee7c2f41a5d97327ccea851faa694050e65fe
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 12:15:22 -0400

    game module

---
+++ src/modules/Game.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { heartRateStore } from "$lib/store";
+
+  let heartRate = 0;
+  heartRateStore.subscribe((value) => {
+    heartRate = value;
+  });
+</script>
+
+<section class="p-4 bg-slate-300 rounded-md">
+  <h1>Game</h1>
+  <p>reading the heartrate from a different component: {heartRate}</p>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Game from "../modules/Game.svelte";
   import HeartRateInput from "./HeartRateInput.svelte";
 </script>
 
 <div class="grid grid-cols-1 gap-4">
   <HeartRateInput />
+  <Game />
 </div>
