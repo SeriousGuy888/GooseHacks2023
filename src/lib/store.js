4e6e57057d0b0f02a765e3addb9c360439faa4d1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 12:16:31 -0400

    Update store.js

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,3 +1,10 @@
+/*
+  This is the store used to store global state data, such as the heart rate.
+  It is used to pass data between components.
+
+  Docs: https://svelte.dev/docs/svelte-store
+*/
+
 import { writable } from "svelte/store";
 
 export const heartRateStore = writable(0);
