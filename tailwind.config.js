7bfae5f16974591ea92a97bbfb546fac3f598e98
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 23:25:19 -0400

    use montserrat as font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
 };
-
