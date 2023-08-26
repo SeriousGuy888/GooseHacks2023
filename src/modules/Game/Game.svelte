70a1cb449869cfec666dd934653313f6b65bb4ff
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-26 02:23:56 -0400

    put start workout button in center

--- src/modules/Game/Game.svelte
+++ src/modules/Game/Game.svelte
@@ -36,9 +36,9 @@
     <GameCanvas bind:this={canvas} />
     <!-- <HeartRateInput /> -->
   {:else}
-    <div class="p-4">
+    <div class="grid place-items-center py-16 px-8">
       <button
-        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        class="bg-green-500 hover:bg-green-700 text-white text-4xl font-bold py-8 px-16 rounded-xl"
         on:click={startGame}
       >
         Start Workout
