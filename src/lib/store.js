8df157d6b00042da45a9bb68d330ac571ca2b91f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-25 23:25:30 -0400

    allow buying upgrades

--- src/lib/store.js
+++ src/lib/store.js
@@ -10,14 +10,33 @@
     $heartRate // automatically update when the store is updated.
 */
 
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 
 export const heartRate = writable(80);
 export const heartRateHistory = writable([]);
 
 export const points = writable(0);
 
-
 // the currency used for upgrades
-export const gold = writable(0)
+export const gold = writable(1000);
 
+// The number of each upgrade already purchased
+export const upgrades = writable({
+  gold: 0,
+  damage: 0,
+  critChance: 0,
+});
+
+// Multipliers derived from upgrades
+export const multipliers = derived(upgrades, ($upgrades) => ({
+  gold: Math.round((1 + $upgrades.gold * 0.1) * 10) / 10,
+  damage: Math.round((1 + $upgrades.damage * 0.1) * 10) / 10,
+  critChance: Math.round((1 + $upgrades.critChance * 0.1) * 10) / 10,
+}));
+
+// Costs derived from upgrades
+export const upgradeCosts = derived(upgrades, ($upgrades) => ({
+  gold: Math.round(10 * Math.pow(1.1, $upgrades.gold)),
+  damage: Math.round(10 * Math.pow(1.1, $upgrades.damage)),
+  critChance: Math.round(10 * Math.pow(1.1, $upgrades.critChance)),
+}));
--- src/routes/upgrades/+page.svelte
+++ src/routes/upgrades/+page.svelte
@@ -1,7 +1,66 @@
 <script>
-  import { gold } from "$lib/store";
+  // @ts-nocheck
+
+  import { gold, upgrades, multipliers, upgradeCosts } from "$lib/store";
+
+  const availableUpgrades = Object.keys($upgrades);
+
+  const upgradeInfo = {
+    gold: {
+      title: "Extra Gold",
+      description: "Increase the amount of gold dropped by enemies.",
+    },
+    damage: {
+      title: "Extra Damage",
+      description: "Increase the amount of damage you deal to enemies.",
+    },
+    critChance: {
+      title: "Criticals",
+      description: "Increase the chance of critical hits.",
+    },
+  };
+
+  /**
+   * @param {string} upgradeKey
+   */
+  function buyUpgrade(upgradeKey) {
+    if ($gold < $upgradeCosts[upgradeKey]) {
+      return;
+    }
+
+    gold.update((g) => g - $upgradeCosts[upgradeKey]);
+    upgrades.update((u) => ({ ...u, [upgradeKey]: u[upgradeKey] + 1 }));
+  }
 </script>
 
-<section class="m-4 max-w-screen-lg">
-  <p>You have {$gold} gold to spend.</p>
-</section>
+<div class="m-4 max-w-screen-lg grid gap-4">
+  <section>
+    <p>You have {$gold} gold to spend.</p>
+  </section>
+
+  <section class="grid gap-3">
+    {#each availableUpgrades as upgradeKey}
+      <div class="p-4 bg-gray-200 rounded-md">
+        <h2 class="font-bold">
+          {upgradeInfo[upgradeKey]?.title}
+          <span class="text-sm">
+            ({$upgrades[upgradeKey]} &middot; {$multipliers[upgradeKey]}x)</span
+          >
+        </h2>
+        <p class="text-gray-700">
+          {upgradeInfo[upgradeKey]?.description}
+        </p>
+
+        <div class="flex justify-end">
+          <button
+            class="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 disabled:bg-gray-500 disabled:cursor-not-allowed"
+            on:click={() => buyUpgrade(upgradeKey)}
+            disabled={$gold < $upgradeCosts[upgradeKey]}
+          >
+            Buy for {$upgradeCosts[upgradeKey]} gold
+          </button>
+        </div>
+      </div>
+    {/each}
+  </section>
+</div>
