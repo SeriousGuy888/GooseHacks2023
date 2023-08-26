1b331115ca666278494bbef6f3c03ba087897dee
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-26 02:08:28 -0400

    gyroscope test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script>
+  import { onMount } from "svelte";
   import Game from "../modules/Game/Game.svelte";
+  import { page } from "$app/stores";
+  import { browser } from "$app/environment";
+
+  if (browser) {
+    if (!window.DeviceOrientationEvent) {
+      console.log("DeviceOrientationEvent not supported.");
+    } else {
+      // @ts-ignore
+      const gyroscope = new Gyroscope({ frequency: 60 });
+
+      gyroscope.addEventListener("reading", (/** @type {any} */ _) => {
+        console.log("Angular velocity along the X-axis " + gyroscope.x);
+        console.log("Angular velocity along the Y-axis " + gyroscope.y);
+        console.log("Angular velocity along the Z-axis " + gyroscope.z);
+      });
+
+      gyroscope.start();
+    }
+  }
 </script>
 
 <Game />
